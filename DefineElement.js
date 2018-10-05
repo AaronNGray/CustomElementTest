e8ae98e31ad64271fbf3c94675e2464c3ccbc9c7
Walle Cyril <cyril.walle@protonmail.com>
2018-10-05 17:33:56 +0200

    Update DefineElement.js

--- DefineElement.js
+++ DefineElement.js
@@ -2,10 +2,6 @@ function kebabToCamel(name) {
     name = name.replace(/(\-\w)/g, function(m) { return m[1].toUpperCase(); });
     return name.charAt(0).toUpperCase() + name.slice(1)
 }
-function evalInContext(js, context) {
-    context = context || {};
-    return function() { return eval(js); }.call(context);
-}
 
 class DefineElement extends HTMLElement {
 
@@ -19,7 +15,7 @@ class DefineElement extends HTMLElement {
         console.log('DefineElement::connectedCallback()');
         var name = this.getAttribute('name');
         var className = kebabToCamel(name);
-        var classObject = evalInContext(className);
+        var classObject = elementClasses[className];
         console.log("  Register class ", className, " as Custom Element ", name);
 
         window.customElements.define(name, classObject);
